e08c8587e8f964ff622a05b12f2c43603f6622b8
Robert Regan <robert_regan86@yahoo.com>
2021-06-03 20:04:49 -0400

    commented out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
-import Header from './Header';
-import SectionMain from './SectionMain';
-import Aside from './Aside';
-import Footer from './Footer';
+// this lets us use react - also using class based components
+import './App.css'; // allows us to use css
 
+import Header from './Header'; // different components
+import SectionMain from './SectionMain'; // ^
+import Aside from './Aside'; // ^
+import Footer from './Footer'; // ^
+
+// creates class based component
 class App extends Component {
-
+// our page
   render() {
     return (
       <div className="App">
@@ -20,3 +23,4 @@ class App extends Component {
 }
 
 export default App;
+// export so it can be used in index.js
