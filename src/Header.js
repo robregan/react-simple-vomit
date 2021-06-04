bb23688d4a714cec00fa4542332ba4358553c7c4
Robert Regan <robert_regan86@yahoo.com>
2021-06-03 20:55:46 -0400

    gotta come back for comments

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React, { Component } from 'react'; // so we can use react
+import './Header.css'; // bring in header
 
+// making a header component
 class Header extends Component {
   render() {
     const style = {
-      width: this.props.width,
-      backgroundColor: this.props.backColor
+      width: this.props.width, // this refers to this component-header 
+      backgroundColor: this.props.backColor // kinda have an idea 
     }
     return (
-        <header style={style} className="Header-main">
+        <header style={style} className="Header-main">// a little confusing still 
         </header>
 
     );
   }
 }
-
-export default Header;
 
 
 //document.querySelector("header").style.backgroundColor = "red"
