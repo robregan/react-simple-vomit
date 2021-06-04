d27d7084d1b265baf6f77c1d4d0de4f976c91e7d
Robert Regan <robert_regan86@yahoo.com>
2021-06-03 20:22:35 -0400

    commented!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+// so we can use react
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// react dom is virtual dom lets us modify specific nodes and make changes quickly
+import './index.css'; // styling
+import App from './App'; //
+import registerServiceWorker from './registerServiceWorker'; 
+// think ^ deals with trying to prevent  loss of connectivity 
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// renders our app with id of root, from index.html in our public folder
+registerServiceWorker(); // fires off the function
+ 
